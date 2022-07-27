819513a51e7f32b9aa9af4b921de75eee6117f53
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-07-27 20:49:59 +0800

    Finished feed

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function searching(){
-    document.getElementById("myDiv").style.backgroundColor = "red";
+
+function favorite(){
+    document.getElementById("favorite").style.filter="invert(88%) sepia(34%) saturate(6897%) hue-rotate(3deg) brightness(106%) contrast(108%)";
+}
+function dislike(){
+    document.getElementById("dislike").style.filter="invert(12%) sepia(96%) saturate(4344%) hue-rotate(10deg) brightness(104%) contrast(118%)";
 }
